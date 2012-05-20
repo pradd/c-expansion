74155c264711af539a77762a30ce490e1557134a
Pradd <pradd@tut.by>
2012-05-21 01:02:45 +0300

    drop some unused crap from Utils

--- src/CExpansion/Utils.hs
+++ src/CExpansion/Utils.hs
@@ -8,12 +8,3 @@ split (x:y:xs) = ( x : fst (split xs), y : snd (split xs) )
 split3 l = (xs, ys, zs)
   where (xs, qs) = split l
         (ys, zs) = split qs
-        
--- индусский код
-intToInteger :: Int -> Integer
-intToInteger x = read (show x)
-
--- ещё индусский код 8)
-intToFloat :: Int -> Float
-intToFloat x = read (show x)
-
